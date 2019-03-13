e5565c57a0abac46b8a0cd7fc81d5e941ad7efa9
Peter Lockett <cerwym@googlemail.com>
2019-03-13 10:47:59 +0000

    Skeleton with required dev dependencies

--- src/core/App.js
+++ src/core/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const {dispatch} = this.props;
+  }
+
   render() {
     return (
       <div className="App">
